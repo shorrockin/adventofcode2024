e5180b47b823407ae4f71878159c07af2e43f657
Chris Shorrock <cs@stripe.com>
2024-11-30 21:20:11 -0800

    day 01

--- pkg/day01/solve.go
+++ pkg/day01/solve.go
@@ -1,5 +1,48 @@
 package day01
 
-func Solve(path string) int {
-	return 0
+import (
+	"adventofcode2024/pkg/assert"
+	"adventofcode2024/pkg/utils"
+	"math"
+	"slices"
+	"strings"
+)
+
+type Part int
+
+const (
+	Part1 Part = iota
+	Part2
+)
+
+func Solve(path string, part Part) int {
+	var left []int
+	var right []int
+
+	for _, line := range utils.MustReadInput(path) {
+		ints := strings.Fields(line)
+		assert.Equal(2, len(ints), "expected 2 fields per line", len(ints))
+		left = append(left, utils.MustAtoi(ints[0]))
+		right = append(right, utils.MustAtoi(ints[1]))
+	}
+
+	if part == Part1 {
+		slices.Sort(left)
+		slices.Sort(right)
+
+		sum := 0
+		for idx, l := range left {
+			sum += int(math.Abs(float64(right[idx] - l)))
+		}
+		return sum
+	} else {
+		frequencies := make(map[int]int)
+		for _, r := range right {
+			frequencies[r]++
+		}
+
+		return utils.Reduce(left, 0, func(acc int, l int) int {
+			return acc + (l * frequencies[l])
+		})
+	}
 }
--- pkg/day01/solve_test.go
+++ pkg/day01/solve_test.go
@@ -7,13 +7,17 @@ import (
 )
 
 func TestPartOneExample(t *testing.T) {
-	assert.Equal(t, -1, Solve("input.example.txt"))
+	assert.Equal(t, 11, Solve("input.example.txt", Part1))
 }
 
-// func TestPartOneActual(t *testing.T) {
-// 	assert.Equal(t, -1, Solve("input.txt"))
-// }
+func TestPartTwoExample(t *testing.T) {
+	assert.Equal(t, 31, Solve("input.example.txt", Part2))
+}
 
-// func TestPartTwoActual(t *testing.T) {
-// 	assert.Equal(t, -1, Solve("input.txt"))
-// }
+func TestPartOneActual(t *testing.T) {
+	assert.Equal(t, 1646452, Solve("input.txt", Part1))
+}
+
+func TestPartTwoActual(t *testing.T) {
+	assert.Equal(t, 23609874, Solve("input.txt", Part2))
+}
