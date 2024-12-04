0b89f852eefddb100ca499599b26b66b680e4abb
Chris Shorrock <cs@stripe.com>
2024-12-04 06:28:02 -0800

    day 04 cleanup

--- pkg/day04/solve.go
+++ pkg/day04/solve.go
@@ -3,22 +3,28 @@ package day04
 import (
 	"adventofcode2024/pkg/grid"
 	"adventofcode2024/pkg/utils"
-	"slices"
 )
 
-func PartOne(path string) int {
-	wordSearch := parse(path)
+type Pattern struct {
+	offsets []grid.Coordinate
+	value   string
+}
+
+func Solve(path string, partTwo bool) int {
 	found := 0
+	patterns := generatePatterns(partTwo)
+	wordSearch := grid.Parse(utils.MustReadInput(path), func(value rune, x int, y int) rune {
+		return value
+	})
 
 	for _, node := range wordSearch {
-		for _, direction := range grid.Directions {
-			x := node.Coordinate
-			m := x.Offset(direction)
-			a := m.Offset(direction)
-			s := a.Offset(direction)
+		for _, pattern := range patterns {
+			coordinates := utils.Map(pattern.offsets, func(c grid.Coordinate) grid.Coordinate {
+				return node.Coordinate.Offset(c)
+			})
 
-			if letters, ok := get(wordSearch, x, m, a, s); ok {
-				if string(letters) == "XMAS" {
+			if vlaue, ok := extract(wordSearch, coordinates...); ok {
+				if string(vlaue) == pattern.value {
 					found++
 				}
 			}
@@ -27,52 +33,32 @@ func PartOne(path string) int {
 	return found
 }
 
-func PartTwo(path string) int {
-	wordSearch := parse(path)
-	found := 0
-
-	for _, node := range wordSearch {
-		center := node.Coordinate
-		nw := center.Offset(grid.NorthWest)
-		ne := center.Offset(grid.NorthEast)
-		sw := center.Offset(grid.SouthWest)
-		se := center.Offset(grid.SouthEast)
-
-		if letters, ok := get(wordSearch, center, nw, ne, sw, se); ok {
-			if letters[1] == letters[4] {
-				// opposing corners shouldn't match
-				continue
-			}
-			if letters[0] != 'A' {
-				// 'A' should be in the center
-				continue
-			}
-			slices.Sort(letters)
-			if string(letters) != "AMMSS" {
-				// remaining letters can only be M or S, two of each
-				continue
-			}
-
-			found++
-		}
+func generatePatterns(partTwo bool) []Pattern {
+	if !partTwo {
+		return utils.Map(grid.Directions, func(direction grid.Coordinate) Pattern {
+			x := grid.At(0, 0)
+			m := x.Offset(direction)
+			a := m.Offset(direction)
+			s := a.Offset(direction)
+			return Pattern{[]grid.Coordinate{x, m, a, s}, "XMAS"}
+		})
 	}
-	return found
-}
 
-func parse(path string) grid.Grid[rune] {
-	return grid.Parse(utils.MustReadInput(path), func(value rune, x int, y int) rune {
-		return value
+	return utils.Map([]string{"AMMSS", "ASSMM", "AMSMS", "ASMSM"}, func(value string) Pattern {
+		return Pattern{[]grid.Coordinate{grid.At(0, 0), grid.NorthWest, grid.NorthEast, grid.SouthWest, grid.SouthEast}, value}
 	})
 }
 
-func get(search grid.Grid[rune], coordinates ...grid.Coordinate) ([]rune, bool) {
+func extract(wordSearch grid.Grid[rune], coordinates ...grid.Coordinate) ([]rune, bool) {
 	var contents = make([]rune, len(coordinates))
+
 	for idx, coordinate := range coordinates {
-		if node, ok := search.Get(coordinate); ok {
+		if node, ok := wordSearch.Get(coordinate); ok {
 			contents[idx] = node.Contents
 		} else {
 			return nil, false
 		}
 	}
+
 	return contents, true
 }
--- pkg/day04/solve_test.go
+++ pkg/day04/solve_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 func TestPartOneExample(t *testing.T) {
-	assert.Equal(t, 18, PartOne("input.example.txt"))
+	assert.Equal(t, 18, Solve("input.example.txt", false))
 }
 
 func TestPartOneActual(t *testing.T) {
-	assert.Equal(t, 2427, PartOne("input.txt"))
+	assert.Equal(t, 2427, Solve("input.txt", false))
 }
 
 func TestPartTwoExample(t *testing.T) {
-	assert.Equal(t, 9, PartTwo("input.example.txt"))
+	assert.Equal(t, 9, Solve("input.example.txt", true))
 }
 
 func TestPartTwoActual(t *testing.T) {
-	assert.Equal(t, 1900, PartTwo("input.txt"))
+	assert.Equal(t, 1900, Solve("input.txt", true))
 }
